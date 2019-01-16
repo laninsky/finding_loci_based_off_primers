f39a191d7f1f75024ec900c172932dadfa97f1b8
Alana Alexander <laninsky@gmail.com>
2019-01-16 16:35:42 +1300

    Update finding_loci_based_off_primers.sh

--- finding_loci_based_off_primers.sh
+++ finding_loci_based_off_primers.sh
@@ -1,1 +1,23 @@
-noseqs=`wc -l tempout | awk '{print $1}'`
+genomefile=`tail -n+1 params.txt | head -n1`;
+primerfile=`tail -n+2 params.txt | head -n1`;
+tolerance=`tail -n+3 params.txt | head -n1`;
+
+makeblastdb -in $genomefile -dbtype nucl
+
+noloci=`wc -l $primerfile | awk '{print $1}'`;
+
+bylocus () {
+locusline=`tail -n+1 $primerfile | head -n1`;
+firstseq=`echo $locusline | awk '{print $2}'`
+secondseq=`echo $locusline | awk '{print $3}'`
+echo ${firstseq}"NNNNNNNNNN"${secondseq} > $i.queryseq
+
+blastn -task blastn-short -db $genomefile -query $i.queryseq -outfmt '6 qseqid sseqid sstart send bitscore' > $i.blast
+}
+
+# for-loop allowing bylocus to be parallelized
+for i in `seq 1 $noloci`;
+do bylocus "$i" & done
+wait
+
+Rscript summarising_blast.R
