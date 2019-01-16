1c35631d254f6875ec04236bf4cecee34d32f51d
Alana Alexander <laninsky@gmail.com>
2019-01-17 12:55:52 +1300

    Update summarising_blast.R

--- summarising_blast.R
+++ summarising_blast.R
@@ -1,5 +1,10 @@
 library(readr)
+library(dplyr)
 
 primertable <- read_table2((as.matrix(read_table2("params.txt",col_names=FALSE)[2,1])),col_names=FALSE)
 
 blastresults <- list.files(pattern=".blast")
+
+for (i in 1:length(blastresults)) {
+  tempblastresults <- read_table2(blastresults[i],col_names=FALSE)
+  tempblastresults %>% group_by
