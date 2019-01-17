4c205b674eb0923e89c723fbd56312c4da5678a4
Alana Alexander <laninsky@gmail.com>
2019-01-17 16:53:37 +1300

    Update summarising_blast.R

--- summarising_blast.R
+++ summarising_blast.R
@@ -4,12 +4,12 @@ library(dplyr)
 primertable <- read_table2((as.matrix(read_table2("params.txt",col_names=FALSE)[2,1])),col_names=FALSE)
 length_slush <- as.numeric(read_table2("params.txt",col_names=FALSE)[3,1])
 
-blastresults <- list.files(pattern=".blast")
+blastresults <- list.files(pattern="*.blast$")
 
 output <- as_tibble()
 
 for (i in 1:length(blastresults)) {
-  tempblastresults <- read_table2(blastresults[i],col_names=FALSE)
+  tempblastresults <- read_table2(paste(i,".blast",sep=""),col_names=FALSE)
   
   summarised_matches <- tempblastresults %>% 
     group_by(X2) %>% 
